4c1c601e2ed4b4f463adc09e69bf90bc9cf2b5da
ik-top-digital <top.digital.ik@gmail.com>
2022-05-16 11:45:19 +0300

    Connecting the _Favorite_ Status to Users

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -44,8 +44,9 @@ class Products with ChangeNotifier {
   // var _showFavoritesOnly = false;
 
   final String authToken;
+  final String userId;
 
-  Products(this.authToken, this._items);
+  Products(this.authToken, this.userId, this._items);
 
   List<Product> get items {
     // if (_showFavoritesOnly) {
@@ -73,7 +74,7 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> fetchAndSetProducts() async {
-    final url = Uri.parse(
+    var url = Uri.parse(
         'https://flutter-shop-db-realtime-default-rtdb.firebaseio.com/products.json?auth=$authToken');
     try {
       final response = await http.get(url);
@@ -82,6 +83,11 @@ class Products with ChangeNotifier {
       if (extractedData.isEmpty || extractedData == null) {
         return;
       }
+
+      url = Uri.parse(
+          'https://flutter-shop-db-realtime-default-rtdb.firebaseio.com/userFavorites/$userId.json?auth=$authToken');
+      final favoriteResponse = await http.get(url);
+      final favoriteData = json.decode(favoriteResponse.body);
       final List<Product> loadedProducts = [];
 
       extractedData.forEach((prodId, prodData) {
@@ -92,7 +98,7 @@ class Products with ChangeNotifier {
           title: prodData['title'],
           description: prodData['description'],
           price: prodData['price'],
-          isFavorite: prodData['isFavorite'],
+          isFavorite: favoriteData == null ? false : favoriteData[prodId] ?? false,
           imageUrl: prodData['imageUrl'],
         ));
       });
@@ -114,7 +120,6 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
-          'isFavorite': product.isFavorite,
         }),
       );
       final newProduct = Product(
